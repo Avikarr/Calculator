52432bb68383906b1b49de24be03a12cb6f9b042
Avinash Karotia <122970436+Avikarr@users.noreply.github.com>
2023-07-03 14:16:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let str = "";
+
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '=') {
+            str = eval(str);
+            document.getElementById("input").value = str;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            str = "";
+            document.getElementById("input").value = str;
+        }
+        else {
+            str = str + e.target.innerHTML;
+            document.getElementById("input").value = str;
+        }
+    })
+})
+
+
+
+
+
+setInterval(()=>{
+    
+let date = new Date();
+let timeHour = date.getHours();
+let timeMinute = date.getMinutes();
+// let timeSec = date.getSeconds();
+let time = document.getElementById("time");
+    if (timeHour >= 12) {
+        if (timeMinute < 10) {
+            time.innerHTML = timeHour - 12 + ":" + "0" + timeMinute;
+        }
+        else {
+            time.innerHTML = timeHour - 12 + ":" + timeMinute;
+        }
+    
+    }
+    else {
+        if (timeMinute < 10) {
+            time.innerHTML = timeHour + ":" + "0" + timeMinute;
+        }
+        else {
+            time.innerHTML = timeHour + ":" + timeMinute;
+        }
+    }
+},1000)
+
+
+
